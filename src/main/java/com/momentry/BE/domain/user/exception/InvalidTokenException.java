d77ebd3df8606aa040c1a4b8d7e8c4aa4ee16388
박상찬 <gkgk00142@naver.com>
2026-01-22 09:27:56 +0900

    [feat] OauthValidator 구현체 - google, apple 추가

---
+++ src/main/java/com/momentry/BE/domain/user/exception/InvalidTokenException.java
@@ -0,0 +1,14 @@
+package com.momentry.BE.domain.user.exception;
+
+import com.momentry.BE.global.exception.BusinessException;
+
+public class InvalidTokenException extends BusinessException {
+    
+    public InvalidTokenException() {
+        super("Invalid ID token", 400);
+    }
+    
+    public InvalidTokenException(String message) {
+        super(message, 400);
+    }
+}
