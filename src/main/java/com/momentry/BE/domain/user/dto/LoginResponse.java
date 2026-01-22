66035703cf5f099c8984e2dfdf90997c878f7181
박상찬 <gkgk00142@naver.com>
2026-01-22 09:22:14 +0900

    [feat] Login의 request, response 타입 dto 정의

---
+++ src/main/java/com/momentry/BE/domain/user/dto/LoginResponse.java
@@ -0,0 +1,43 @@
+package com.momentry.BE.domain.user.dto;
+
+import com.momentry.BE.domain.user.entity.AlertPreference;
+import com.momentry.BE.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class LoginResponse {
+
+    private Long userId;
+    private String userName;
+    private String profileUrl;
+    private String email;
+    private String provider;
+    private AlertDto alert;
+    private String accessToken;
+
+    @Getter
+    @Builder
+    public static class AlertDto {
+        private boolean albumCreated;
+        private boolean invited;
+        private boolean fileUploaded;
+
+        public AlertDto(AlertPreference alertPreference){
+            this.albumCreated = alertPreference.getAlbumCreated();
+            this.invited = alertPreference.getInvited();
+            this.fileUploaded = alertPreference.getFileUploaded();
+        }
+    }
+
+    public LoginResponse(User user, AlertPreference alertPreference, String accessToken){
+        this.userId = user.getId();
+        this.userName = user.getUsername();
+        this.profileUrl = user.getProfileImageUrl();
+        this.email = user.getEmail();
+        this.provider = user.getProvider();
+        this.alert = new AlertDto(alertPreference);
+        this.accessToken = accessToken;
+    }
+}
