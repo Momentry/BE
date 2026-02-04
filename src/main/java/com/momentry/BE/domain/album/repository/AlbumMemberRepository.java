651e1a9e625214b7831e31a807bc501379d98f83
Joey <pjhch1009@gmail.com>
2026-02-04 13:10:47 +0900

    fix: 앨범 리스트 조회 시 멤버 프로필개수 제한을 전체에서 각 앨범별로 적용되도록 수정

--- src/main/java/com/momentry/BE/domain/album/repository/AlbumMemberRepository.java
+++ src/main/java/com/momentry/BE/domain/album/repository/AlbumMemberRepository.java
@@ -7,7 +7,6 @@ import com.momentry.BE.domain.album.dto.AlbumCountDto;
 import com.momentry.BE.domain.album.dto.AlbumUrlDto;
 import com.momentry.BE.domain.album.entity.Album;
 import com.momentry.BE.domain.album.entity.MemberAlbumPermission;
-import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,27 +17,30 @@ import com.momentry.BE.domain.album.entity.AlbumMember;
 @Repository
 public interface AlbumMemberRepository extends JpaRepository<AlbumMember, Long> {
 
-    @Query("SELECT am FROM AlbumMember am WHERE am.album.id = :albumId AND am.user.id = :userId")
-    Optional<AlbumMember> findByAlbumIdAndUserId(@Param("albumId") Long albumId, @Param("userId") Long userId);
+        @Query("SELECT am FROM AlbumMember am WHERE am.album.id = :albumId AND am.user.id = :userId")
+        Optional<AlbumMember> findByAlbumIdAndUserId(@Param("albumId") Long albumId, @Param("userId") Long userId);
 
-    @Query("SELECT am FROM AlbumMember am JOIN FETCH am.album WHERE am.album.id = :albumId AND am.user.id = :userId")
-    Optional<AlbumMember> findByAlbumIdAndUserIdWithAlbum(@Param("albumId") Long albumId, @Param("userId") Long userId);
+        @Query("SELECT am FROM AlbumMember am JOIN FETCH am.album WHERE am.album.id = :albumId AND am.user.id = :userId")
+        Optional<AlbumMember> findByAlbumIdAndUserIdWithAlbum(@Param("albumId") Long albumId,
+                        @Param("userId") Long userId);
 
-    @Query("SELECT am FROM AlbumMember am JOIN FETCH am.user WHERE am.album.id = :albumId")
-    List<AlbumMember> findByAlbumIdWithUser(@Param("albumId") Long albumId);
+        @Query("SELECT am FROM AlbumMember am JOIN FETCH am.user WHERE am.album.id = :albumId")
+        List<AlbumMember> findByAlbumIdWithUser(@Param("albumId") Long albumId);
 
-    @Query("SELECT am.album FROM AlbumMember am WHERE am.user.id = :userId")
-    List<Album> findAlbumsByUserId(Long userId);
+        @Query("SELECT am.album FROM AlbumMember am WHERE am.user.id = :userId")
+        List<Album> findAlbumsByUserId(Long userId);
 
-    @Query("SELECT new com.momentry.BE.domain.album.dto.AlbumCountDto(am.album.id, CAST(COUNT(am) AS integer)) FROM AlbumMember am " +
-            "WHERE am.album.id IN :albumIds GROUP BY am.album.id")
-    List<AlbumCountDto> countMembersByAlbumIds(List<Long> albumIds);
+        @Query("SELECT new com.momentry.BE.domain.album.dto.AlbumCountDto(am.album.id, CAST(COUNT(am) AS integer)) FROM AlbumMember am "
+                        +
+                        "WHERE am.album.id IN :albumIds GROUP BY am.album.id")
+        List<AlbumCountDto> countMembersByAlbumIds(List<Long> albumIds);
 
-    @Query("SELECT new com.momentry.BE.domain.album.dto.AlbumUrlDto(am.album.id, am.user.profileImageUrl) FROM AlbumMember am " +
-            "WHERE am.album.id IN :albumIds ORDER BY am.user.username ASC")
-    List<AlbumUrlDto> findMemberProfilesByAlbumIds(List<Long> albumIds, Limit limit);
+        @Query("SELECT new com.momentry.BE.domain.album.dto.AlbumUrlDto(am.album.id, am.user.profileImageUrl) FROM AlbumMember am "
+                        +
+                        "WHERE am.album.id IN :albumIds ORDER BY am.album.id, am.user.username ASC")
+        List<AlbumUrlDto> findMemberProfilesByAlbumIds(@Param("albumIds") List<Long> albumIds);
 
-    @Query("SELECT am.permission FROM AlbumMember am " +
-            "WHERE am.album.id = :albumId AND am.user.id = :userId")
-    Optional<MemberAlbumPermission> findPermissionByAlbumIdAndUserId(Long albumId, Long userId);
+        @Query("SELECT am.permission FROM AlbumMember am " +
+                        "WHERE am.album.id = :albumId AND am.user.id = :userId")
+        Optional<MemberAlbumPermission> findPermissionByAlbumIdAndUserId(Long albumId, Long userId);
 }
