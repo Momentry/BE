6158f846cc7145581ba756b1aae039ae4f98aa03
김준혁 <bears081887@gmail.com>
2026-03-01 15:59:40 +0900

    fix: provider의 대소문자 일치를 위해 enum 클래스에 @JsonCreator 추가

--- src/main/java/com/momentry/BE/domain/user/enums/Provider.java
+++ src/main/java/com/momentry/BE/domain/user/enums/Provider.java
@@ -1,6 +1,13 @@
 package com.momentry.BE.domain.user.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Provider {
     GOOGLE,
-    APPLE
+    APPLE;
+
+    @JsonCreator
+    public static Provider from(String value) {
+        return Provider.valueOf(value.toUpperCase());
+    }
 }
